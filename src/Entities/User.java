e3b5d6d75777dc2574e6429ac4b64a430e5a1073
Mohamed SALLAMI RHOUMA <m.rhouma@sofrecom-tunisie.com>
2022-05-25 11:31:07 +0100

    Create user entity

---
+++ src/Entities/User.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entities;
+
+/**
+ *
+ * @author m.rhouma
+ */
+public class User {
+    private int id;
+    private String name,role,email,password;
+
+    public User(String name, String role, String email, String password) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
