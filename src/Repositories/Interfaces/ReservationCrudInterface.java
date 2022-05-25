71f461cc08173b006e22c2345f0d0e5a0234f17d
Mohamed SALLAMI RHOUMA <m.rhouma@sofrecom-tunisie.com>
2022-05-25 19:40:37 +0100

    Init reservation repo interface

---
+++ src/Repositories/Interfaces/ReservationCrudInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Repositories.Interfaces;
+import Entities.Reservation;
+/**
+ *
+ * @author m.rhouma
+ */
+public interface ReservationCrudInterface extends CrudRepository<Reservation>{
+    
+}
