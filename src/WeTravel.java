82dc0d61d8e167c3536d5d192306774b3f7df44d
tajoutihamza <tajouti.hamza@gmail.com>
2022-05-26 23:53:59 +0100

    test restaurant CRUD

--- src/WeTravel.java
+++ src/WeTravel.java
@@ -1,9 +1,13 @@
 
+import Entities.Restaurants;
 import Entities.User;
 import Entities.hotels;
 import Repositories.HotelsCrudImpl;
-import Repositories.UserCrudImpl;
 import Services.HotelsService;
+import Repositories.Interfaces.IRestaurantsCrud;
+import Repositories.RestaurantsCrud;
+import Repositories.UserCrudImpl;
+import Services.RestaurantService;
 import Services.UserService;
 import Utils.DataSource;
 import java.sql.SQLException;
@@ -13,7 +17,6 @@ import java.util.ArrayList;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 /**
  *
  * @author m.rhouma
@@ -25,33 +28,47 @@ public class WeTravel {
      */
     public static void main(String[] args) throws SQLException {
         // Init Data source
-        DataSource ds =  DataSource.getInstance();
-        
+        DataSource ds = DataSource.getInstance();
+
         // Init repositories
         UserCrudImpl userCrud = new UserCrudImpl(ds.getCon());
-        
+        IRestaurantsCrud restaurantsCrud = new RestaurantsCrud(ds.getCon());
+        HotelsCrudImpl hotelCrud = new HotelsCrudImpl(ds.getCon());
+
         // Init services
         UserService userService = new UserService(userCrud);
-        
+        RestaurantService restaurantService = new RestaurantService(restaurantsCrud);
+        HotelsService hotelService = new HotelsService(hotelCrud);
+
         // Tests
-        User user = new User("Mohamed","ADMIN","moihamed@wetravel.com","mohamed123","22001002");
+        User user = new User("Mohamed", "ADMIN", "moihamed@wetravel.com", "mohamed123", "22001002");
         userService.create(user);
         ArrayList users = userService.getAll();
+
         System.out.println(users);
-        
-        // Hotels
-        // Init repositories d'hotel
-        HotelsCrudImpl hotelCrud = new HotelsCrudImpl(ds.getCon());
-        
-        // Init services d'hotel
-        HotelsService hotelService = new HotelsService(hotelCrud);
-        
+
         // Tests d'hotel
-        hotels hotel = new hotels("el mouradi",4,100,"elmouradi@Sousse.com","Sousse","Tunisie");
+        hotels hotel = new hotels("el mouradi", 4, 100, "elmouradi@Sousse.com", "Sousse", "Tunisie");
         hotelService.create(hotel);
         ArrayList hotelss = hotelService.getAll();
         System.out.println(hotelss);
-        
+
+        Restaurants restaurant = new Restaurants(70, "Lac 2", "Tunis", "Tunisie", "via mercato");
+        Restaurants restaurant2 = new Restaurants(50, "Lac 1", "Tunis", "Tunisie", "New Food");
+        Restaurants restaurant3 = new Restaurants(60, "Lac 1", "Tunis", "Tunisie", "Good Food");
+
+        restaurantService.createRestaurant(restaurant);
+        restaurantService.createRestaurant(restaurant2);
+
+        restaurantService.updateRestaurants(2, restaurant3);
+
+        restaurantService.deleteRestaurants(3);
+
+        Restaurants resto = restaurantService.findRestaurants(1);
+        ArrayList restaurants = restaurantService.findAllRestaurants();
+
+        System.out.println(resto);
+        System.out.println(restaurants);
     }
-    
+
 }
