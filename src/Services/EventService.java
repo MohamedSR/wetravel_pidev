bb83091c08ce2a3a19617c5130b60f64d0ef89e9
Mouhib TRABELSI <mh.trabelsi@sofrecom-tunisie.com>
2022-05-29 20:47:17 +0100

    add event crud

---
+++ src/Services/EventService.java
@@ -0,0 +1,36 @@
+package Services;
+
+import Repositories.Interfaces.EventCrudInterface;
+import java.sql.SQLException;
+import Entities.Event;
+import java.util.ArrayList;
+
+
+public class EventService {
+
+    private final EventCrudInterface eventCrud;
+
+    public EventService(EventCrudInterface eventCrud) {
+        this.eventCrud = eventCrud;
+    }
+
+    public ArrayList getAllEvents() throws SQLException {
+        return eventCrud.findAll();
+    }
+
+    public Event getEvent(int id) throws SQLException {
+        return eventCrud.find(id);
+    }
+
+    public void createEvent(Event event) throws SQLException {
+        eventCrud.create(event);
+    }
+
+    public void updateEvent(int id,Event event) throws SQLException {
+        eventCrud.update(id,event);
+    }
+
+    public void deleteEvent(int id) throws SQLException {
+        eventCrud.delete(id);
+    }
+}
