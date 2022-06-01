c1519956d604f594aac409a4d6f2c606b48557ea
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-06-01 16:24:41 +0100

    implement menu class

--- src/Entities/Menu.java
+++ src/Entities/Menu.java
@@ -1,4 +1,51 @@
 package Entities;
 
 public class Menu {
+    private int id;
+    private Restaurants restaurants;
+    private String name;
+
+    public Menu(int id, Restaurants restaurants, String name) {
+        this.id = id;
+        this.restaurants = restaurants;
+        this.name = name;
+    }
+
+    public Menu(Restaurants restaurants, String name) {
+        this.restaurants = restaurants;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "id=" + id +
+                ", restaurants=" + restaurants +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Restaurants getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(Restaurants restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
