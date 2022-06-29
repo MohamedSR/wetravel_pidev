9a9a933ef533c6f5137e003746655d4bd84bcf1f
tajoutihamza <tajouti.hamza@gmail.com>
2022-06-30 00:19:55 +0100

    add image

---
+++ src/Utils/UploadImage.java
@@ -0,0 +1,31 @@
+package Utils;
+
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class UploadImage {
+
+
+    public static String selectImage(ActionEvent event,String imgPath) {
+        FileChooser fileChooser = new FileChooser();
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterJPG
+                = new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+        FileChooser.ExtensionFilter extFilterjpg
+                = new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+        FileChooser.ExtensionFilter extFilterPNG
+                = new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+        FileChooser.ExtensionFilter extFilterpng
+                = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+        fileChooser.getExtensionFilters()
+                .addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+        imgPath = file.getAbsolutePath();
+        return imgPath;
+    }
+
+
+}
